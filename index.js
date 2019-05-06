3a959305b2a6b5a259172ac4ff70dc54845fbce4
Carlos Lopez <clo2go@gmail.com>
2019-05-06 16:27:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ function happyHolidaysTo(name){
   return (`Happy holidays, ${name}!`)
 }
 
+function happyHolidayTo(holiday, name){
+  return (`Happy ${holiday}, ${name}!`)
+}
+
 console.log(happyHolidays());
 console.log(happyHolidaysTo('Alex'));
